d27ed9781ff78ab33f320fd1fac5f6e513bbd29f
Havsalt <knutolaihelgesen@gmail.com>
2024-06-16 23:01:34 +0200

    Reworked shell

--- cl/shell.py
+++ cl/shell.py
@@ -1,38 +1,126 @@
+from __future__ import annotations
+
+import subprocess
 import pathlib
 import shlex
+import json
+import re
 import os
 from argparse import ArgumentParser
+from typing import TypeVar, TypeAlias, Generic
 
-from actus import info, warn
+import strox
 import colex
+from actus import warn, LogSection
 
-from . import (
-    shorthand,
-    win_commands,
-    intake
+from .intake import get_live_input
+
+T = TypeVar("T")
+class Stack(list[T], Generic[T]): ...
+Command: TypeAlias = list[str]
+
+
+KEYWORD_PREFIX = "!"
+KEYWORD_INSERT_PATTERN = re.compile(
+    r"(![-\w]+)[\s\\]?$",
+    flags=re.RegexFlag.ASCII
 )
+KEYWORD_UNMATCHED_PATTERN = re.compile(
+    r"(![-\w]+)[\s\\]?$",
+    flags=re.RegexFlag.ASCII
+)
+import io
+dev = LogSection("Dev", fd=io.StringIO()) # DEV: suppress with `fd=io.StringIO()`
+
+
+def expand_keywords(string: str) -> str:
+    fd = (
+        pathlib.Path(__file__)
+        .parent
+        .joinpath("substitutions.json")
+        .open()
+    )
+    substitutions: dict[str, str] = json.load(fd)
+    fd.close()
+
+    def replace(match: re.Match[str]) -> str:
+        keyword = match.group()[1:]
+        if keyword not in substitutions:
+            return match.group()
+        return substitutions[keyword]
+    
+    # substitue keywords with their replacements if possible
+    for keyword in sorted(substitutions.keys(), key=len, reverse=True):
+        string = re.sub(KEYWORD_INSERT_PATTERN, replace, string)
+    # warn non-expanded keywords
+    for unmatched in re.finditer(KEYWORD_UNMATCHED_PATTERN, string):
+        keyword = unmatched.group()[1:]
+        closest = strox.get_closest_match(
+            keyword,
+            substitutions.keys()
+        )
+        with warn(f"No keyword replacement found for $[{keyword}]"):
+            warn(f"Closest match was $[{closest}]")
+    return string
+
+
+def parse_command(string: str) -> Stack[Command]:
+    # NOTE: Stack implementation with only decending layers
+    # TODO: make a Tree implementation for layer contiunation
+    parts = shlex.split(string, posix=False)
+    dev(f"Parts: {parts}")
+    if not parts:
+        warn("Command has no segments")
+    elif parts[0] == "|":
+        warn('Command started with $["|"]')
+    stack: Stack[Command] = Stack()
+    command = []
+    for part in parts:
+        if part == "|":
+            stack.append(command)
+            command = []
+            continue
+        command.append(part)
+    if command:
+        stack.append(command)
+    dev(f"Sub: {stack}")
+    # TODO: structure for tree iterations
+    # initial command (caller ish)
+    # arg - "|" is result of subcommand, layer down
+    # (in "|") - "<|" is layer up subcommand
+    return stack
+
+
+def execute_stack(stack: Stack[Command]) -> str | None:
+    result: str | None = None
+    while stack:
+        command = stack.pop(-1)
+        if result is not None:
+            command.append(result)
+        dev(f"Command: {command}")
+        process = subprocess.run(command, text=True, shell=True, capture_output=True)
+        result = process.stdout.rstrip("\n")
+        dev(f"Stdout: {result}")
+    dev(f"Result: {result}")
+    return result
 
 
 def enter_session(context: ArgumentParser) -> None:
-    parser = context # alias
-    # main loop
-    info("Started 'cl' session")
-    options = win_commands.collect()
     while True:
-        # raw_command = input("Enter command: ")
-        prompt = f"{colex.colorize('(Cl)', colex.SEA_GREEN)} {pathlib.Path.cwd().stem}: "
-        raw_command = intake.get_live_input(prompt, options=options)
-        command_parts = shlex.split(raw_command)
-        match command_parts:
-            case ["exit", *_]:
-                info(f"Exited '{parser.usage}' session")
-                exit(0)
-            case [parser.usage]: # "cl"
-                warn(f"Cannot run command '{parser.usage}' from '{parser.usage}' session")
-                continue
-            case [command, *arguments]:
-                substituted_arguments = shorthand.substitute(arguments)
-                final_command = command + " " + " ".join(substituted_arguments)
-                os.system(final_command)
-            case [command]:
-                os.system(raw_command)
+        prompt = (
+            colex.colorize("Enter command", colex.SALMON)
+            + colex.colorize(":", colex.WHITE)
+            + " "
+        )
+        raw_command = get_live_input(prompt).lstrip()
+        dev(f"Raw: {raw_command}")
+        if raw_command in ("exit", "."):
+            return
+        elif raw_command == "cls":
+            os.system("cls")
+            continue
+
+        expanded = expand_keywords(raw_command)
+        command_stack = parse_command(expanded)
+        result = execute_stack(command_stack)
+        print(result)
