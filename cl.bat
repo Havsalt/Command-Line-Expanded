046ed035c3e9c70e34d11bba8683ce0ea154e6ca
Havsalt <knutolaihelgesen@gmail.com>
2024-06-18 16:52:04 +0200

    Updated run script

--- cl.bat
+++ cl.bat
@@ -1,7 +1,25 @@
 @echo off
-set ORIGIN=%CD%
-cd /d %~dp0
-call .venv\Scripts\activate.bat
+call %~dp0\.venv\Scripts\activate.bat
 python -m cl %*
-cd /d %ORIGIN%
-deactivate
+
+@REM deactivate script from .venv\Scripts\deactivate.bat
+@set VIRTUAL_ENV=
+@set VIRTUAL_ENV_PROMPT=
+
+@REM Don't use () to avoid problems with them in %PATH%
+@if not defined _OLD_VIRTUAL_PROMPT @goto ENDIFVPROMPT
+    @set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+    @set _OLD_VIRTUAL_PROMPT=
+:ENDIFVPROMPT
+
+@if not defined _OLD_VIRTUAL_PYTHONHOME @goto ENDIFVHOME
+    @set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+    @set _OLD_VIRTUAL_PYTHONHOME=
+:ENDIFVHOME
+
+@if not defined _OLD_VIRTUAL_PATH @goto ENDIFVPATH
+    @set "PATH=%_OLD_VIRTUAL_PATH%"
+    @set _OLD_VIRTUAL_PATH=
+:ENDIFVPATH
+
+exit /b %ERRORLEVEL%
