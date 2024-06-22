df7adf37e4becdc4f75c7e16d5448163d3ce8751
Havsalt <knutolaihelgesen@gmail.com>
2024-06-22 17:12:06 +0200

    Fixed deletion of whole words when ran from cmd

--- cl/intake.py
+++ cl/intake.py
@@ -13,7 +13,7 @@ _SPACE = " "
 _ENTER = b"\r"
 _TAB = b"\t"
 _DELETE = b"\x08"
-_DELETE_WORD = b"\x17"
+_DELETE_WORD = (b"\x17", b"\x7f")
 _CONTROL_C = b"\x03"
 
 _ARROW_MODIFIER = b"\xe0"
@@ -76,7 +76,7 @@ def get_live_input( # TODO: add History param
                 _sys.stdout.write(move_code)
                 _sys.stdout.flush()
         
-        elif key_code == _DELETE_WORD:
+        elif key_code in _DELETE_WORD:
             ignore_first = bool(written) and written[-1] == _SPACE
             while written and (written[-1] != _SPACE or ignore_first):
                 if ignore_first:
@@ -93,6 +93,7 @@ def get_live_input( # TODO: add History param
         key = key_code.decode()
         if key not in _WHITELISTED:
             continue
+        
         if key not in (_TAB, _DELETE): # ENTER pressed
             if written:
                 remove_code = f"\u001b[{len(written)}D"
@@ -101,24 +102,29 @@ def get_live_input( # TODO: add History param
             cursor += 1
             _sys.stdout.write(written)
             _sys.stdout.flush()
+        
         if key == _TAB and written:
             pattern = written.split()[0] # TODO: move upward, for live recommendations
             predicted = _get_closest_match(pattern, options, budget=_BUDGET)
             remove_code = f"\u001b[{len(written)}D"
             fill_code = predicted.ljust(len(written), " ")
-            move_code = (f"\u001b[{len(written) - len(predicted)}D"
-                         if written and (len(written) - len(predicted) > 0)
-                         else "")
+            move_code = (
+                f"\u001b[{len(written) - len(predicted)}D"
+                if written and (len(written) - len(predicted) > 0)
+                else ""
+            )
             written = predicted
             _sys.stdout.write(remove_code)
             _sys.stdout.write(fill_code)
             _sys.stdout.write(move_code)
             _sys.stdout.flush()
+        
         # TODO: write and flush
         # _sys.stdout.write("\n---")
         # _sys.stdout.write("\n" + written)
         # _sys.stdout.write("\n" + predicted)
         # _sys.stdout.write("\n===\n")
+    
     _sys.stdout.write(end)
     _sys.stdout.flush()
     return written
